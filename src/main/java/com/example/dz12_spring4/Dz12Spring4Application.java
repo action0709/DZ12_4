021b11494140d0b8da7c53a7df6aa981d604e7dd
action0709 <action0709@mail.ru>
2023-06-26 17:52:56 +0400

    1

---
+++ src/main/java/com/example/dz12_spring4/Dz12Spring4Application.java
@@ -0,0 +1,13 @@
+package com.example.dz12_spring4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz12Spring4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz12Spring4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dz12_spring4/Dz12Spring4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz12_spring4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz12Spring4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
